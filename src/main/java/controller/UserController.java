48fc72317f5110036113633fc350bd80e94fa863
zhengjiabin <zhengjiabin6@126.com>
2015-07-19 23:31:28 +0800

    ceshi

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -6,6 +6,7 @@ import model.UserModel;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import serv.UserServ;
@@ -22,8 +23,47 @@ public class UserController {
         return model;
     }
     
-    @RequestMapping("/getUser")
-    public String getUser(String id, HttpServletRequest request) {
+    /**
+     * http://localhost:8080/mybatis_springmvc/user/getUser1.do?id=1
+     * 
+     * @param id
+     * @param request
+     * @return
+     */
+    
+    public String getUser1(String id, HttpServletRequest request) {
+        UserModel model = serv.getUserModel(Integer.parseInt(id));
+        request.setAttribute("userModel", model);
+        
+        return "user";
+    }
+    
+    /**
+     * http://localhost:8080/mybatis_springmvc/user/getUser2/1.do
+     * 
+     * @param id
+     * @param request
+     * @return
+     */
+    @RequestMapping("/getUser/{id}")
+    public String getUser2(@PathVariable
+    String id, HttpServletRequest request) {
+        UserModel model = serv.getUserModel(Integer.parseInt(id));
+        request.setAttribute("userModel", model);
+        
+        return "user";
+    }
+    
+    /**
+     * http://localhost:8080/mybatis_springmvc/user/1/getUser.do
+     * 
+     * @param id
+     * @param request
+     * @return
+     */
+    @RequestMapping("/{id}/getUser")
+    public String getUser(@PathVariable
+    String id, HttpServletRequest request) {
         UserModel model = serv.getUserModel(Integer.parseInt(id));
         request.setAttribute("userModel", model);
         
